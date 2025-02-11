c6df422beb15df8d171a8d28f185aec4538b28a0
Swayy123456789 <dan.borko@seznam.cz>
2025-02-11 13:51:19 +0100

    delani navbaru

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
+import "./home.css"
+import "./navbar.css"
 
 import React from 'react'
 
 export default function Home() {
   return (
     <>
-        <Link to={"/add-car"}>
-            <p>Add car</p>
+    <header>
+      <img class="logo" src="" alt="logo"></img>
+      <nav>
+        <ul class="nav_links">
+          <li><a href="#"/>Products</li>
+          <li><a href="#"/>Contact</li>
+          <li><a href="#"/>About us</li>
+        </ul>
+      </nav>
+      <a class="cta" href="#">Admin mode</a>
+    </header>
+        <Link to={"/add-product"}>
+            <p>Add product</p>
         </Link>
 
-        <Link to={"/view-cars"}>
-            <p>View car</p>
+        <Link to={"/view-product"}>
+            <p>View product</p>
         </Link>
     </>
   )
