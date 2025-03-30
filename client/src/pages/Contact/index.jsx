4763cb1f194977ab28335f7dd2b60faa2428e647
Daniel Borko <dan.borko@seznam.cz>
2025-03-30 21:32:40 +0200

    contact us changes

--- client/src/pages/Contact/index.jsx
+++ client/src/pages/Contact/index.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Header from "../../components/Header/index";
 import Footer from "../../components/Footer/Footer";
 import MiniMap from "../../components/MiniMap/MiniMap";
+import { Facebook } from "lucide-react";
+import { Twitter } from "lucide-react";
+import { Instagram } from "lucide-react";
 
 
 
@@ -32,11 +35,12 @@ export default function Contact() {
   return (
     <>
       <Header />
-      <section className="max-w-6xl !px-6 !py-12 text-white !mx-auto">
+      <section className="backdrop-blur-lg bg-opacity-40 !mx-auto max-w-390 !rounded-lg">
+      <section className="!max-w-320 !px-6 !py-25 text-white !mx-auto !my-15">
         
         <div className="!grid !grid-cols-1 !md:grid-cols-2 !gap-8">
           {/* Formulář */}
-          <div className="backdrop-blur-lg bg-opacity-40 p-8 rounded-lg shadow-lg border border-gray-700">
+          <div className="backdrop-blur-lg bg-opacity-40 !p-8 rounded-lg shadow-lg border border-white ">
             <h3 className="text-xl font-bold text-white !mb-6 !py-4 text-center">
               Máte dotaz? Napište nám!
             </h3>
@@ -49,10 +53,10 @@ export default function Contact() {
               </div>
             ) : (
               <form onSubmit={handleSubmit}>
-                <div className="mb-4">
+                <div className="!mb-4">
                   <label
                     htmlFor="fullName"
-                    className="block text-sm font-medium text-gray-400"
+                    className="!block text-sm font-bold text-gray-400"
                   >
                     Jméno
                   </label>
@@ -63,7 +67,7 @@ export default function Contact() {
                     value={formData.fullName}
                     onChange={handleChange}
                     required
-                    className="w-full px-4 py-2 mt-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
+                    className="!w-full !px-4 !py-2 !mt-2 backdrop-blur-lg bg-opacity-40 text-white border !border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
                   />
                 </div>
 
@@ -81,14 +85,14 @@ export default function Contact() {
                     value={formData.email}
                     onChange={handleChange}
                     required
-                    className="w-full px-4 py-2 mt-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
+                    className="!w-full !px-4 !py-2 !mt-2 backdrop-blur-lg bg-opacity-40 text-white border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
                   />
                 </div>
 
                 <div className="mb-4">
                   <label
                     htmlFor="subject"
-                    className="block text-sm font-medium text-gray-400"
+                    className="!block !text-sm font-bold text-gray-400"
                   >
                     Předmět
                   </label>
@@ -97,7 +101,7 @@ export default function Contact() {
                     name="subject"
                     value={formData.subject}
                     onChange={handleChange}
-                    className="w-full px-4 py-2 mt-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
+                    className="!w-full !px-4 !py-2 !mt-2 backdrop-blur-lg bg-opacity-40 text-white border border-gray-500 rounded-md !focus:ring-2 focus:ring-indigo-500 !outline-none"
                   >
                     <option value="">Vyberte předmět</option>
                     <option value="Dotaz na produkt">Dotaz na produkt</option>
@@ -107,10 +111,10 @@ export default function Contact() {
                   </select>
                 </div>
 
-                <div className="mb-4">
+                <div className="!mb-4">
                   <label
                     htmlFor="message"
-                    className="block text-sm font-medium text-gray-400"
+                    className="!block !text-sm font-bold text-gray-400"
                   >
                     Zpráva
                   </label>
@@ -120,14 +124,14 @@ export default function Contact() {
                     value={formData.message}
                     onChange={handleChange}
                     required
-                    rows="5"
-                    className="w-full px-4 py-2 mt-2 bg-gray-800 text-white border border-gray-700 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
+                    rows="4"
+                    className="!w-full !px-4 !py-2 !mt-2 backdrop-blur-lg bg-opacity-40 text-white border border-gray-500 rounded-md focus:ring-2 focus:ring-indigo-500 outline-none"
                   ></textarea>
                 </div>
 
                 <button
                   type="submit"
-                  className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                  className="!w-full !py-4 !px-4 bg-indigo-600 text-white rounded-md !hover:bg-white focus:outline-none focus:ring-2 !focus:ring-white"
                 >
                   Odeslat zprávu
                 </button>
@@ -136,56 +140,68 @@ export default function Contact() {
           </div>
 
           {/* Kontakt. údaje */}
-          <div className="bg-gray-900 p-8 rounded-lg shadow-lg border border-gray-700">
-            <h3 className="text-xl font-medium text-gray-300 mb-6">
+          <div className="backdrop-blur-lg bg-opacity-40 !p-8 rounded-lg !shadow-lg border border-gray-400 !py-10 !my-20">
+            <h3 className="text-xl font-bold text-white !mb-6">
               Kontaktní údaje
             </h3>
-            <div className="mb-4">
-              <p className="font-bold text-gray-300">Telefon</p>
+            <div className="!mb-4">
+              <p className="font-bold text-white">Telefon</p>
               <p className="text-gray-400">+420 735 847 770</p>
             </div>
 
-            <div className="mb-4">
-              <p className="font-bold text-gray-300">E-mail</p>
+            <div className="!mb-4">
+              <p className="font-bold text-white">E-mail</p>
               <p className="text-gray-400">watchtime@seznam.cz</p>
             </div>
 
-            <div className="mb-4">
-              <p className="font-bold text-gray-300">
+            <div className="!mb-4">
+              <p className="font-bold text-white">
                 Adresa pro vrácení zboží
               </p>
               <p className="text-gray-400">Pařížská, 110 00 Praha 1</p>
             </div>
 
-            <div className="!mb-4">
+            <div className="!mb-6">
               <p className="font-bold text-gray-300">Otevírací doba</p>
               <p className="text-gray-400">Pondělí - Pátek: 10:00 - 18:00</p>
               <p className="text-gray-400">Sobota - Neděle: Zavřeno</p>
             </div>
 
             <div>
-              <p className="font-bold text-gray-300 !mb-20">
+
+              <p className="font-bold text-gray-300 !mb-17">
                 Sledujte nás na sociálních sítích
               </p>
-              <div className="flex space-x-4">
-                <a href="#" className="text-indigo-400 hover:text-indigo-500">
-                  Facebook
+
+              <div className="!flex !space-x-20 !text-center">
+
+                <a href="https://www.facebook.com/?locale=cs_CZ" className="text-indigo-400">
+                <Facebook />
                 </a>
-                <a href="#" className="text-indigo-400 hover:text-indigo-500">
-                  Instagram
+
+                <a href="https://x.com/i/flow/login" className="text-indigo-400">
+                <Twitter />
                 </a>
-                <a href="#" className="text-indigo-400 hover:text-indigo-500">
-                  TikTok
+
+                <a href="https://www.instagram.com/" className="text-indigo-400">
+                <Instagram />
                 </a>
-                <a href="#" className="text-indigo-400 hover:text-indigo-500">
-                  LinkedIn
-                </a>
+            <div className="!mx-70 !-my-82"> 
+                <MiniMap />
+                </div>
+
               </div>
+
             </div>
+
           </div>
+
         </div>
+
       </section>
-      <MiniMap />
+
+      </section>
+      
       <Footer />
     </>
   );
